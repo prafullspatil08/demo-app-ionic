496ce341fcbc686bbd836d82c74ec5d174391469
rushikesh-salunke <rushikesh.salunke@mindbowser.com>
2023-08-31 18:26:52 +0530

    generated todo form

--- src/app/feature/todo-list/todo-list.component.ts
+++ src/app/feature/todo-list/todo-list.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent  implements OnInit {
+export class TodoListComponent implements OnInit {
+  todoForm: FormGroup;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {
+    this.initializeForm();
+  }
 
-  ngOnInit() {}
+  initializeForm() {
+    this.todoForm = new FormGroup({
+      title: new FormControl('', [Validators.required]),
+      amount: new FormControl(null, [Validators.required]),
+      date: new FormControl('', [Validators.required]),
+    });
+  }
 
+  addExpenseForm() {}
 }
